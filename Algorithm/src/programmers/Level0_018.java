4495ce1cbece5ee59dfa75cdee2bd9343931326d
황인재 <hij2696@hanyang.ac.kr>
2023-02-13 02:12:40 +0900

    Programmers | Level0_017 ~ Level0_020

---
+++ Algorithm/src/programmers/Level0_018.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        int coffee_num = 0;
+        
+        while (money >= 5500) {
+            coffee_num++;
+            money -= 5500;
+        }
+        
+        answer[0] = coffee_num;
+        answer[1] = money;
+        
+        return answer;
+    }
+}
