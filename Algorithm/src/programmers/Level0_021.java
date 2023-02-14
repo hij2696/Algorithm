e5a33dd14f1d07d170f71b58bfb7b9ef1785d24f
황인재 <hij2696@hanyang.ac.kr>
2023-02-14 16:24:37 +0900

    Programmers | Level0_021 ~ Level0_024

---
+++ Algorithm/src/programmers/Level0_021.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        char[] arr = my_string.toCharArray();
+        char[] reversedArr = new char[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversedArr[arr.length - 1 - i] = arr[i];
+        }
+        
+        String answer = new String(reversedArr);
+        
+        return answer;
+    }
+}
