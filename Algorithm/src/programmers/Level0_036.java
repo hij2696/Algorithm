1490ccfa1e142dbcc202e75f483aad67f19fee73
황인재 <hij2696@hanyang.ac.kr>
2023-02-17 16:43:13 +0900

    Programmers | Level0_033 ~ Level0_036

---
+++ Algorithm/src/programmers/Level0_036.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int len1 = num_list.length / n;
+        int len2 = num_list.length / len1;
+        
+        int[][] answer = new int[len1][len2];
+        int k = 0;
+        
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                answer[i][j] += num_list[i + j + k];
+            }
+            k =  k + len2 - 1;
+        }
+        
+        return answer;
+    }
+}
