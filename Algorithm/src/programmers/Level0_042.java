56eb258d4ae757326bcaad07166016cd11e85919
황인재 <hij2696@hanyang.ac.kr>
2023-02-22 21:39:53 +0900

    Programmers | Level0_042 ~ Level0_044

---
+++ Algorithm/src/programmers/Level0_042.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        
+        for (int i = 0; i < sides.length - 1; i++) {
+            for (int j = i + 1; j < sides.length; j++) {
+                if (sides[i] < sides[j]) {
+                    int tmp = sides[i];
+                    sides[i] = sides[j];
+                    sides[j] = tmp;
+                }
+            }
+        }
+        
+        if (sides[0] < sides[1] + sides[2]) answer = 1;
+        else answer = 2;
+        
+        return answer;
+    }
+}
