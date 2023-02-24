43c9bca94c553badb4b01dac2c6b68f7d8963675
황인재 <hij2696@hanyang.ac.kr>
2023-02-24 15:37:41 +0900

    Programmers | Level0_061 ~ Level0_064

---
+++ Algorithm/src/programmers/Level0_063.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] c = my_string.toCharArray();
+        
+        char tmp = c[num1];
+        c[num1] = c[num2];
+        c[num2] = tmp;
+        
+        String answer = String.valueOf(c);
+        
+        return answer;
+    }
+}
