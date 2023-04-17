0588e3c6550f06ee30a7b0fcf7a8f4a43b1b8730
황인재 <hij2696@hanyang.ac.kr>
2023-04-17 20:16:58 +0900

    boj | 11023, 11382

---
+++ Algorithm/src/baekjoon/boj_11382.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+		BigInteger C = new BigInteger(st.nextToken());
+		
+		bw.write(A.add(B.add(C)) + "\n");
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
