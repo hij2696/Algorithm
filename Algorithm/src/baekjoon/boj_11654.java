7a88c250ccede66300d2ffa593dacf7fb376260f
황인재 <hij2696@hanyang.ac.kr>
2023-04-18 22:18:29 +0900

    boj | 11654, 11718

---
+++ Algorithm/src/baekjoon/boj_11654.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		char x;
+		
+		x = (char)System.in.read();
+		
+		System.out.print((int)x);
+	}
+
+}
