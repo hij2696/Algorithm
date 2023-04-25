9fd93a400f24bca70a4d7cf7ca631c36d7121e6c
황인재 <hij2696@hanyang.ac.kr>
2023-04-25 20:25:13 +0900

    boj | 15964, 16170

---
+++ Algorithm/src/baekjoon/boj_15964.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static long sum(long a, long b) {
+		return (a + b) * (a - b);
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		
+		bw.write(sum(A, B) + "\n");
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
---
+++ Algorithm/src/baekjoon/boj_16170.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("2023");
+		System.out.println("04");
+		System.out.print("25");
+		
+	}
+
+}
