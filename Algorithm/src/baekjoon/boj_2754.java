7fed50064d70771b8bd01b7542c73271ae7a7944
황인재 <hij2696@hanyang.ac.kr>
2023-07-14 23:40:16 +0900

    boj | 2754, 25083, 27866

---
+++ Algorithm/src/baekjoon/boj_2754.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next(); 
+		
+		switch (str) {
+		case "A+": System.out.print("4.3");
+					break;
+		case "A0": System.out.print("4.0");
+					break;
+		case "A-": System.out.print("3.7");
+					break;
+		case "B+": System.out.print("3.3");
+					break;
+		case "B0": System.out.print("3.0");
+					break;
+		case "B-": System.out.print("2.7");
+					break;
+		case "C+": System.out.print("2.3");
+					break;
+		case "C0": System.out.print("2.0");
+					break;
+		case "C-": System.out.print("1.7");
+					break;
+		case "D+": System.out.print("1.3");
+					break;
+		case "D0": System.out.print("1.0");
+					break;
+		case "D-": System.out.print("0.7");
+					break;
+		case "F": System.out.print("0.0");
+					break;
+		default: System.out.print("Invalid value");
+					break;
+		}	
+        sc.close();
+	}
+}
