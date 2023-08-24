909e49f5ac3552d5df0ef5d384c79cbb604e9398
황인재 <hij2696@hanyang.ac.kr>
2023-08-24 23:35:30 +0900

    boj | 24267

---
+++ Algorithm/src/baekjoon/boj_24267.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+ 
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        long n = Long.parseLong(br.readLine()) - 2;
+        
+        bw.write(String.valueOf(((n * n * n) + (3 * n * n) + (2 * n)) / 6) +"\n3");
+        
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+ 
+}
