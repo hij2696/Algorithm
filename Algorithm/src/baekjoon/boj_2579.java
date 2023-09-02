74b9c9a93bd4d671bd3fe4f4a5aea6ab977ad2a7
황인재 <hij2696@hanyang.ac.kr>
2023-09-02 14:20:11 +0900

    boj | 2579

---
+++ Algorithm/src/baekjoon/boj_2579.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int N = Integer.parseInt(br.readLine());
+    	int[] dp = new int[N + 1];
+    	int[] arr = new int[N + 1];
+    	
+    	for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(br.readLine());
+    	
+    	dp[1] = arr[1];
+    	if (N >= 2) dp[2] = arr[1] + arr[2];
+    	
+    	for (int i = 3; i <= N; i++) {
+    		dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+    	}
+    	
+    	bw.write(String.valueOf(dp[N]));
+    	
+    	bw.flush();
+    	br.close();
+    	bw.close();
+    }
+}
