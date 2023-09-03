dc3f709456a7ffb4cdcd52fd49654a2ff79e23db
황인재 <hij2696@hanyang.ac.kr>
2023-09-03 09:13:48 +0900

    boj | 2156, 2217, 11053, 11055, 11722, 11727

---
+++ Algorithm/src/baekjoon/boj_11055.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	StringTokenizer st = null;
+    	
+    	int A = Integer.parseInt(br.readLine());
+    	int[] arr = new int[A];
+    	int[] dp = new int[A];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for (int i = 0; i < A; i++) arr[i] = Integer.parseInt(st.nextToken());
+    	
+    	for (int i = 0; i < A; i++) {
+    		dp[i] = arr[i];
+    		for (int j = 0; j < i; j++) {
+    			if (arr[i] > arr[j]) {
+    				dp[i] = Math.max(dp[j] + arr[i], dp[i]);
+    			}
+    		}
+    	}
+    	
+    	int max = 0;
+    	for (int i = 0; i < A; i++) {
+    		max = dp[i] > max ? dp[i] : max;
+    	}
+    	
+    	bw.write(String.valueOf(max));
+    	
+    	bw.flush();
+    	br.close();
+    	bw.close();
+    }
+}
---
+++ Algorithm/src/baekjoon/boj_11727.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	int[] dp = new int[1001];
+    	
+    	dp[1] = 1;
+    	dp[2] = 3;
+    	
+    	for (int i = 3; i <= n; i++) {
+    		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+    	}
+    	
+    	bw.write(String.valueOf(dp[n]));
+    	
+    	bw.flush();
+    	br.close();
+    	bw.close();
+    }
+}
