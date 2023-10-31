948285d7614f6562b882e83f62e6a0a3f9c5107b
황인재 <hij2696@hanyang.ac.kr>
2023-11-01 02:42:05 +0900

    Programmers | Level1_부족한 금액 계산하기

---
+++ Algorithm/src/programmers/Level1_26.java
@@ -0,0 +1,12 @@
+// 부족한 금액 계산하기
+
+class Solution {
+    public long solution(int price, int money, int count) {
+        long sum = 0;
+        
+        for (int i = 1; i <= count; i++) sum += price * i;
+        
+        if (money - sum >= 0) return 0;
+        else return sum - money;
+    }
+}
