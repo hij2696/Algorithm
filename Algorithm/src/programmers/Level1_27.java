893409dcafa3b66041c1ece50b7c111debaf8542
황인재 <hij2696@hanyang.ac.kr>
2023-11-02 17:25:02 +0900

    Programmers | Level1_문자열 다루기 기본

---
+++ Algorithm/src/programmers/Level1_27.java
@@ -0,0 +1,14 @@
+// 문자열 다루기 기본
+
+class Solution {
+    public boolean solution(String s) {
+        if (s.length() != 4 && s.length() != 6) return false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') continue;
+            else return false;
+        }
+        
+        return true;
+    }
+}
